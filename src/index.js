96fad82afcbaabc12984811bcc557fb3440cf95e
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2021-11-21 18:59:22 -0300

    Updating index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import Visualizer from "./lib/visualizer.js";
 import glossary from "./lib/glossary.js";
 
-const whitepaperVisualizer = new Visualizer('./data/whitepaper.jsonl');
-const academicVisualizer = new Visualizer('./data/academic.jsonl');
-const wikiVisualizer = new Visualizer('./data/wiki.jsonl');
+const whitepaperVisualizer = new Visualizer('./data/scierc/whitepaper.jsonl');
+const academicVisualizer = new Visualizer('./data/scierc/academic.jsonl');
+const wikiVisualizer = new Visualizer('./data/scierc/wiki.jsonl');
+
+const whitepaperVisualizerLightweight = new Visualizer('./data/scierc_lightweight/whitepaper.jsonl');
+const academicVisualizerLightweight = new Visualizer('./data/scierc_lightweight/academic.jsonl');
+const wikiVisualizerLightweight = new Visualizer('./data/scierc_lightweight/wiki.jsonl');
 
 whitepaperVisualizer.initializeEntityDict();
 academicVisualizer.initializeEntityDict();
 wikiVisualizer.initializeEntityDict();
 
-let glossaryEntities = whitepaperVisualizer.getGlossaryEntitiesWithAlias();
-console.log(`Detected ${Object.keys(glossaryEntities).length} out of ${glossary.length} terms as entities on whitepaper corpus (${(Object.keys(glossaryEntities).length / glossary.length).toFixed(2) * 100}%)`)
+whitepaperVisualizerLightweight.initializeEntityDict();
+academicVisualizerLightweight.initializeEntityDict();
+wikiVisualizerLightweight.initializeEntityDict();
+
+console.log(academicVisualizer.entityDict['next - generation cryptocurrencies'].relations)
+// console.log('\n')
+// console.log(academicVisualizerLightweight.entityDict['smart-contract'].relations)

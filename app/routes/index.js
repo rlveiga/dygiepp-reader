766dc5c5767a63422984acaa40353725d6bcea2f
Rodrigo Veiga <rodrigoveiga1206@gmail.com>
2021-12-09 17:52:20 -0300

    Updating routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,36 +1,52 @@
 var express = require('express');
+const glossary = require('../lib/glossary');
 const Visualizer = require('../lib/visualizer');
 var router = express.Router();
 
 const whitepaperVisualizer = new Visualizer('./lib/data/scierc/whitepaper.jsonl');
 whitepaperVisualizer.initializeEntityDict();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+const academicVisualizer = new Visualizer('./lib/data/scierc/academic.jsonl');
+academicVisualizer.initializeEntityDict();
+
+const wikiVisualizer = new Visualizer('./lib/data/scierc/wiki.jsonl');
+wikiVisualizer.initializeEntityDict();
+
+const visualizerMap = {
+  whitepaper: whitepaperVisualizer,
+  academic: academicVisualizer,
+  wiki: wikiVisualizer
+}
+
+router.get('/data/:corpus_name/:entity_name', async function (req, res, next) {
+  const visualizer = visualizerMap[req.params.corpus_name];
+  const entity_name = req.params.entity_name;
+
+  res.send(visualizer.entityDict[entity_name] || null);
 });
 
-router.get('/data', async function (req, res, next) {
+router.get('/documents/:corpus_name', async function (req, res, next) {
+  const visualizer = visualizerMap[req.params.corpus_name];
+
   res.send({
-    frequencyList: whitepaperVisualizer.getFrequencyList(),
-    entityDict: whitepaperVisualizer.entityDict,
-    glossary: whitepaperVisualizer.getGlossaryEntities()
+    documentKeys: visualizer.getDocumentKeys()
   });
 });
 
-router.get('/documents', async function (req, res, next) {
-  res.send({
-    documentKeys: whitepaperVisualizer.getDocumentKeys()
-  });
-});
-
-router.get('/documents/:doc_key', async function (req, res, next) {
+router.get('/documents/:corpus_name/:doc_key', async function (req, res, next) {
+  const visualizer = visualizerMap[req.params.corpus_name];
   const doc_key = req.params.doc_key;
 
-  const documentInfo = whitepaperVisualizer.getDocumentInfo(doc_key);
+  const documentInfo = visualizer.getDocumentInfo(doc_key);
 
   res.send({
     data: documentInfo
+  });
+});
+
+router.get('/glossary/', async function (req, res, next) {
+  res.send({
+    glossary: glossary
   });
 });
 
